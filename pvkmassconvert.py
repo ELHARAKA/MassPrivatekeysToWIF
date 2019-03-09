5ecceca7d14b582f2f5e5dcdc6f2f35cf134683a
Fahd El Haraka <35965906+ELHARAKA@users.noreply.github.com>
2019-03-09 19:56:18 +0100

    Add files via upload
    This tool convert private keys bitcoin hexadecimal format to WIF for import on Wallet, accept massive volume, millions of keys at once.
    
    Use:
    1) Copy and Past all private keys in hexadecimal format in the file called "brute-pvks.txt". Paste all private keys in this file.
    2) Now just run the python script pvkmassconvert_compressed.py
    3) That's all, the script will export all privatekeys converted in WIF format in a new text file with name "list-WIF.txt" in the same directory that you ran the script.
    
    *If this helped you, leave a tip :)
    BTC: 1Diqsha8MaLeqGBe11VqxRWTLbEAc8sWtj
    ETH: 0x81d67d8A8B1877987eA52B5FE2C11F39eD7d0D0c
    LTC: Li4jMRYPzzhZnhAbqgU9at9fyZFGFpLKps
    DOG: DKCFDJbshygG8wzn5EoH3B662Hcajtbnqq

---
+++ pvkmassconvert.py
@@ -0,0 +1,36 @@
+
+import binascii, hashlib, base58, sys
+arq = open('list-WIF.txt', 'w')
+
+def convert(z):
+    # Step 1: get the privatekey in extended format, this is hexadecimal upper or lower case.
+    private_key_static = z
+    # Step 2: adding 80 in the front for select de MAINNET channel bitcoin address
+    extended_key = "80"+private_key_static
+    # Step 3: first process SHA-256
+    first_sha256 = hashlib.sha256(binascii.unhexlify(extended_key)).hexdigest()
+    # Step 4: second process SHA-256
+    second_sha256 = hashlib.sha256(binascii.unhexlify(first_sha256)).hexdigest()
+    # Step 5-6: add checksum info to end of extended key
+    final_key = extended_key+second_sha256[:8]
+    # Step 7: finally the Wallet Import Format (WIF) is generated in the format base 58 encode of final_key
+    WIF = base58.b58encode(binascii.unhexlify(final_key))
+    # Step 8: show the private key on usual format WIF for wallet import. Enjoy!
+    print ("Private Key on WIF format below")
+    print (WIF)
+    arq.write("%s \n" % WIF)
+
+with open("brute-pvks.txt") as file:
+    for line in file:
+        print str.strip(line)
+        convert(str.strip(line))
+
+print ("_________________________________")
+print ("Donations for BTC: 13dSqMNqWf6if1hUJ3gkzircgaAHsDLzs3")
+
+
+
+#just run with Python 2.7 for more compatibility
+
+#Usage
+#python.exe pvkmassconvert.py

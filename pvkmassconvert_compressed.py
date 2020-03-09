d327c105de512949100cc71f214f8646fe9a8356
Fahd El Haraka <35965906+ELHARAKA@users.noreply.github.com>
2020-03-09 02:05:41 +0100

    Fix

--- pvkmassconvert_compressed.py
+++ pvkmassconvert_compressed.py
@@ -1,14 +1,20 @@
-
 import binascii, hashlib, base58, sys
 arq = open('list-WIF.txt', 'w')
 
 def convert(z):
+
     private_key_static = z
+
     extended_key = "80"+private_key_static+"01"
+
     first_sha256 = hashlib.sha256(binascii.unhexlify(extended_key)).hexdigest()
+
     second_sha256 = hashlib.sha256(binascii.unhexlify(first_sha256)).hexdigest()
+
     final_key = extended_key+second_sha256[:8]
+
     WIF = base58.b58encode(binascii.unhexlify(final_key)).decode ('ascii')
+
     print ("Private Key on WIF Compressed format below")
     print (WIF)
     arq.write("%s \n" % WIF)
@@ -18,5 +24,7 @@ with open("brute-pvks.txt") as file:
         print (str.strip(line))
         convert(str.strip(line))
 
-print ("_________________________________")
-print ("Donations for BTC: 18jYnQeS8jcgsXNdGcZW3k16NCBYP9419F")
+print ("__________________________________________________\n")
+print ("Developed by: Fahd El Haraka")
+print ("If this saved you time or helped, donations please for BTC Address:")
+print ("1A5Mhbitq2UN95wSm54QQn2PBZSbpFWamu")

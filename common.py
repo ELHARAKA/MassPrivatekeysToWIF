58d4c23b2961122c4776af2610243fec402c486b
Fahd El Haraka <elharakafahd@icloud.com>
2023-10-29 14:01:58 +0000

    Create common.py
    Signed-off-by: Fahd El Haraka <elharakafahd@icloud.com>

---
+++ common.py
@@ -0,0 +1,9 @@
+"""
+Common function used in both compressed and uncompressed files.
+"""
+
+def process_file(file_path, conversion_function):
+    with open(file_path, encoding='utf-8') as file_handle:
+        for line in file_handle:
+            print(str.strip(line))
+            conversion_function(str.strip(line))

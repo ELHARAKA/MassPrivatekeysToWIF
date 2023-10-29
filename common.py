83a19af1c6b90b500998118a8539230cb45a81a9
Fahd El Haraka <elharakafahd@icloud.com>
2023-10-29 14:03:11 +0000

    Update common.py
    Signed-off-by: Fahd El Haraka <elharakafahd@icloud.com>

--- common.py
+++ common.py
@@ -3,6 +3,9 @@ Common function used in both compressed and uncompressed files.
 """
 
 def process_file(file_path, conversion_function):
+    """
+    Process a file with a specified conversion function.
+    """
     with open(file_path, encoding='utf-8') as file_handle:
         for line in file_handle:
             print(str.strip(line))

145a09bc5ae2f8afa7477bc5e691ca311666e18a
Fahd El Haraka <elharakafahd@icloud.com>
2023-10-29 13:19:40 +0000

    Update pvkmassconvert_compressed.py
    Signed-off-by: Fahd El Haraka <elharakafahd@icloud.com>

--- pvkmassconvert_compressed.py
+++ pvkmassconvert_compressed.py
@@ -1,30 +1,23 @@
 import binascii, hashlib, base58, sys
-arq = open('list-WIF.txt', 'w')
 
 def convert(z):
-
-    private_key_static = z
-
-    extended_key = "80"+private_key_static+"01"
-
-    first_sha256 = hashlib.sha256(binascii.unhexlify(extended_key)).hexdigest()
-
-    second_sha256 = hashlib.sha256(binascii.unhexlify(first_sha256)).hexdigest()
-
-    final_key = extended_key+second_sha256[:8]
-
-    WIF = base58.b58encode(binascii.unhexlify(final_key)).decode ('ascii')
-
-    print ("Private Key on WIF Compressed format below")
-    print (WIF)
-    arq.write("%s \n" % WIF)
+    with open('list-WIF.txt', 'a') as arq:  # Updated line
+        private_key_static = z
+        extended_key = "80" + private_key_static + "01"
+        first_sha256 = hashlib.sha256(binascii.unhexlify(extended_key)).hexdigest()
+        second_sha256 = hashlib.sha256(binascii.unhexlify(first_sha256)).hexdigest()
+        final_key = extended_key + second_sha256[:8]
+        WIF = base58.b58encode(binascii.unhexlify(final_key)).decode('ascii')  # Updated line
+        print("Private Key on WIF Compressed format below")
+        print(WIF)
+        arq.write(f"{WIF} \n")  # Updated line
 
 with open("brute-pvks.txt") as file:
     for line in file:
-        print (str.strip(line))
+        print(str.strip(line))  # Updated line
         convert(str.strip(line))
 
-print ("__________________________________________________\n")
-print ("Developed by: Fahd El Haraka")
-print ("If this saved you time or helped, donations please for BTC Address:")
-print ("1S75z47qUK9DehuKQ3qtu4r5U25PxZcCm")
+print("__________________________________________________\n")
+print("Developed by: Fahd El Haraka")
+print("If this saved you time or helped, donations please for BTC Address:")
+print("bc1qttzkk555p78dyq9l8g7syza6n94ppysv66dps0")

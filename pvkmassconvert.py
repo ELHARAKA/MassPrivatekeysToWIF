2ecf5b1c876bd2390f5d7cac86b947d52db7cefc
Fahd El Haraka <elharakafahd@icloud.com>
2023-10-29 13:19:12 +0000

    Update pvkmassconvert.py
    Signed-off-by: Fahd El Haraka <elharakafahd@icloud.com>

--- pvkmassconvert.py
+++ pvkmassconvert.py
@@ -1,22 +1,21 @@
-
 import binascii, hashlib, base58, sys
-arq = open('list-WIF.txt', 'w')
 
 def convert(z):
-    private_key_static = z
-    extended_key = "80"+private_key_static
-    first_sha256 = hashlib.sha256(binascii.unhexlify(extended_key)).hexdigest()
-    second_sha256 = hashlib.sha256(binascii.unhexlify(first_sha256)).hexdigest()
-    final_key = extended_key+second_sha256[:8]
-    WIF = base58.b58encode(binascii.unhexlify(final_key))
-    print ("Private Key on WIF format below")
-    print (WIF)
-    arq.write("%s \n" % WIF)
+    with open('list-WIF.txt', 'a') as arq:  # Updated line
+        private_key_static = z
+        extended_key = "80" + private_key_static
+        first_sha256 = hashlib.sha256(binascii.unhexlify(extended_key)).hexdigest()
+        second_sha256 = hashlib.sha256(binascii.unhexlify(first_sha256)).hexdigest()
+        final_key = extended_key + second_sha256[:8]
+        WIF = base58.b58encode(binascii.unhexlify(final_key))
+        print("Private Key on WIF format below")
+        print(WIF)
+        arq.write(f"{WIF} \n")  # Updated line
 
 with open("brute-pvks.txt") as file:
     for line in file:
-        print str.strip(line)
+        print(str.strip(line))  # Updated line
         convert(str.strip(line))
 
-print ("_________________________________")
-print ("Donations for BTC: 1S75z47qUK9DehuKQ3qtu4r5U25PxZcCm")
+print("_________________________________")
+print("Donations for BTC: bc1qttzkk555p78dyq9l8g7syza6n94ppysv66dps0")
